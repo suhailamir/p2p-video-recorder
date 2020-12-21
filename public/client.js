34034eb69eb37e56302a8f6ddfcaec47722b7203
muhammad <suhail.amir101@gmail.com>
2020-12-21 12:36:39 +0400

    Added recording

--- public/client.js
+++ public/client.js
@@ -1,300 +1,192 @@
 //our username 
-var name; 
+var name;
 var connectedUser;
-  
+
 //connecting to our signaling server
 var conn = new WebSocket('ws://localhost:9090');
-  
-conn.onopen = function () { 
-   console.log("Connected to the signaling server"); 
+
+conn.onopen = function () {
+   console.log("Connected to the signaling server");
 };
-  
+
 //when we got a message from a signaling server 
-conn.onmessage = function (msg) { 
+conn.onmessage = function (msg) {
    console.log("Got message", msg.data);
-   
-   var data = JSON.parse(msg.data); 
-   
-   switch(data.type) { 
+
+   var data = JSON.parse(msg.data);
+
+   switch (data.type) {
       case "login":
-         handleLogin(data.success); 
-         break; 
+         handleLogin(data.success);
+         break;
       //when somebody wants to call us 
-      case "offer": 
-         handleOffer(data.offer, data.name); 
-         break; 
-      case "answer": 
-         handleAnswer(data.answer); 
-         break; 
+      case "offer":
+         handleOffer(data.offer, data.name);
+         break;
+      case "answer":
+         handleAnswer(data.answer);
+         break;
       //when a remote peer sends an ice candidate to us 
-      case "candidate": 
-         handleCandidate(data.candidate); 
-         break; 
-      case "leave": 
-         handleLeave(); 
-         break; 
-      default: 
-         break; 
+      case "candidate":
+         handleCandidate(data.candidate);
+         break;
+      case "leave":
+         handleLeave();
+         break;
+      default:
+         break;
    }
 };
-  
-conn.onerror = function (err) { 
-   console.log("Got error", err); 
+
+conn.onerror = function (err) {
+   console.log("Got error", err);
 };
-  
+
 //alias for sending JSON encoded messages 
-function send(message) { 
+function send(message) {
    //attach the other peer username to our messages 
-   if (connectedUser) { 
-      message.name = connectedUser; 
-   } 
-   
-   conn.send(JSON.stringify(message)); 
+   if (connectedUser) {
+      message.name = connectedUser;
+   }
+
+   conn.send(JSON.stringify(message));
 };
-  
+
 //****** 
 //UI selectors block 
 //******
- 
-var loginPage = document.querySelector('#loginPage'); 
-var usernameInput = document.querySelector('#usernameInput'); 
-var loginBtn = document.querySelector('#loginBtn'); 
 
-var callPage = document.querySelector('#callPage'); 
+var loginPage = document.querySelector('#loginPage');
+var usernameInput = document.querySelector('#usernameInput');
+var loginBtn = document.querySelector('#loginBtn');
+
+var callPage = document.querySelector('#callPage');
 var callToUsernameInput = document.querySelector('#callToUsernameInput');
-var callBtn = document.querySelector('#callBtn'); 
+var callBtn = document.querySelector('#callBtn');
 
 var hangUpBtn = document.querySelector('#hangUpBtn');
-  
-var localVideo = document.querySelector('#localVideo'); 
-var remoteVideo = document.querySelector('#remoteVideo'); 
 
-var yourConn; 
+var localVideo = document.querySelector('#localVideo');
+var remoteVideo = document.querySelector('#remoteVideo');
+
+var yourConn;
 var stream;
-  
+
 callPage.style.display = "none";
 
 // Login when the user clicks the button 
-loginBtn.addEventListener("click", function (event) { 
+loginBtn.addEventListener("click", function (event) {
    name = usernameInput.value;
-   
-   if (name.length > 0) { 
-      send({ 
-         type: "login", 
-         name: name 
-      }); 
-   }
-   
-});
-  
-function handleLogin(success) { 
-   if (success === false) { 
-      alert("Ooops...try a different username"); 
-   } else { 
-      loginPage.style.display = "none"; 
-      callPage.style.display = "block";
-      
-      //********************** 
-      //Starting a peer connection 
-      //********************** 
-      
-      //getting local video stream
-      // navigator.userMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia; 
-      // navigator.userMedia({ video: true, audio: true }, function (myStream) { 
-      //    stream = myStream; 
-         
-      //    //displaying local video stream on the page 
-      //    // localVideo.src = window.URL.createObjectURL(stream);
 
-      //    const leftVideo = document.getElementById('leftVideo');
-
-      //    leftVideo.addEventListener('canplay', () => {
-      //    let stream;
-      //    const fps = 0;
-      //    if (leftVideo.captureStream) {
-      //       stream = leftVideo.captureStream(fps);
-      //    } else if (leftVideo.mozCaptureStream) {
-      //       stream = leftVideo.mozCaptureStream(fps);
-      //    } else {
-      //       console.error('Stream capture is not supported');
-      //       stream = null;
-      //    }
-      //    rightVideo.srcObject = stream;
-      //    });
-      //    localVideo.srcObject=stream;
-      //    localVideo.play();
-         
-      //    //using Google public stun server 
-      //    var configuration = { 
-      //       "iceServers": [{ "url": "stun:stun2.1.google.com:19302" }]
-      //    }; 
-         
-      //    if(!!window.chrome && !!window.chrome.webstore){
-      //      yourConn =  new webkitRTCPeerConnection(configuration); 
-      //    }else{
-      //       yourConn = new RTCPeerConnection(configuration);
-      //    }
-         
-      //    // setup stream listening 
-      //    yourConn.addStream(stream); 
-         
-      //    //when a remote user adds stream to the peer connection, we display it 
-      //    yourConn.onaddstream = function (e) { 
-      //       // remoteVideo.srcObject = window.URL.createObjectURL(e.stream); 
-      //       remoteVideo.srcObject = e.stream; 
-
-      //    };
-         
-      //    // Setup ice handling 
-      //    yourConn.onicecandidate = function (event) { 
-      //       if (event.candidate) { 
-      //          send({ 
-      //             type: "candidate", 
-      //             candidate: event.candidate 
-      //          }); 
-      //       } 
-      //    };  
-         
-      // }, function (error) { 
-      //    console.log(error); 
-      // }); 
-
-
-
-      // /////////////////// Local video stream
-
-            //    //using Google public stun server 
-         var configuration = { 
-            "iceServers": [{ "url": "stun:stun2.1.google.com:19302" }]
-         }; 
-         
-         if(!!window.chrome && !!window.chrome.webstore){
-           yourConn =  new webkitRTCPeerConnection(configuration); 
-         }else{
-            yourConn = new RTCPeerConnection(configuration);
-         }
-
-         // let stream;
-
-
-         // const leftVideo = document.getElementById('localVideoTag');
-
-         // localVideoTag.addEventListener('canplay', () => {
-         // const fps = 0;
-         // console.log('capture stream...')
-         // if (localVideoTag.captureStream) {
-         //    stream = localVideoTag.captureStream(fps);
-         // } else if (localVideoTag.mozCaptureStream) {
-         //    stream = localVideoTag.mozCaptureStream(fps);
-         // } else {
-         //    console.error('Stream capture is not supported');
-         //    stream = null;
-         // }
-
-
-
-         // });
-
-         const videoDOM = $('#localVideoTag')[0]    
-         const videoStream = videoDOM.captureStream(60)
-         localVideo.srcObject=videoStream;
-         localVideo.play();
-         
-         // setup stream listening 
-         yourConn.addStream(videoStream); 
-         
-         //when a remote user adds stream to the peer connection, we display it 
-         yourConn.onaddstream = async (e)=>{ 
-            // remoteVideo.srcObject = window.URL.createObjectURL(e.stream); 
-            remoteVideo.srcObject = e.stream;
-       
-           const sleep = m => new Promise(r => setTimeout(r, m));
-           await sleep(2000);
-           recorder();
-       
-
-         };
-         
-         // Setup ice handling 
-         yourConn.onicecandidate = function (event) { 
-            if (event.candidate) { 
-               send({ 
-                  type: "candidate", 
-                  candidate: event.candidate 
-               }); 
-            } 
-         };  
-      
-   } 
-};
-  
-//initiating a call 
-callBtn.addEventListener("click", function () { 
-   var callToUsername = callToUsernameInput.value;
-   
-   if (callToUsername.length > 0) { 
-   
-      connectedUser = callToUsername;
-      
-      // create an offer 
-      yourConn.createOffer(function (offer) { 
-         send({ 
-            type: "offer", 
-            offer: offer 
-         }); 
-         
-         yourConn.setLocalDescription(offer); 
-      }, function (error) { 
-         alert("Error when creating an offer"); 
+   if (name.length > 0) {
+      send({
+         type: "login",
+         name: name
       });
-      
-   } 
-});
-  
-//when somebody sends us an offer 
-function handleOffer(offer, name) { 
-   connectedUser = name; 
-   yourConn.setRemoteDescription(new RTCSessionDescription(offer));
-   
-   //create an answer to an offer 
-   yourConn.createAnswer(function (answer) { 
-      yourConn.setLocalDescription(answer); 
-      
-      send({ 
-         type: "answer", 
-         answer: answer 
-      }); 
-      
-   }, function (error) { 
-      alert("Error when creating an answer"); 
-   }); 
-};
-  
-//when we got an answer from a remote user
-function handleAnswer(answer) { 
-   yourConn.setRemoteDescription(new RTCSessionDescription(answer)); 
-};
-  
-//when we got an ice candidate from a remote user 
-function handleCandidate(candidate) { 
-   yourConn.addIceCandidate(new RTCIceCandidate(candidate)); 
-};
-   
-//hang up 
-hangUpBtn.addEventListener("click", function () { 
+   }
 
-   send({ 
-      type: "leave" 
-   });  
-   
-   handleLeave(); 
 });
-  
-function handleLeave() { 
-   connectedUser = null; 
-   remoteVideo.src = null; 
-   
-   yourConn.close(); 
-   yourConn.onicecandidate = null; 
-   yourConn.onaddstream = null; 
+
+function handleLogin(success) {
+   if (success === false) {
+      alert("Ooops...try a different username");
+   } else {
+      loginPage.style.display = "none";
+      callPage.style.display = "block";
+      var configuration = {
+         "iceServers": [{ "url": "stun:stun2.1.google.com:19302" }]
+      };
+
+      if (!!window.chrome && !!window.chrome.webstore) {
+         yourConn = new webkitRTCPeerConnection(configuration);
+      } else {
+         yourConn = new RTCPeerConnection(configuration);
+      }
+      const videoDOM = $('#localVideoTag')[0]
+      const videoStream = videoDOM.captureStream(60)
+      localVideo.srcObject = videoStream;
+      localVideo.play();
+      // setup stream listening 
+      yourConn.addStream(videoStream);
+      //when a remote user adds stream to the peer connection, we display it 
+      yourConn.onaddstream = async (e) => {
+         remoteVideo.srcObject = e.stream;
+         const remoteVideoDOM = $('#remoteVideo')[0]
+         const remoteVideoStream = remoteVideoDOM.captureStream(60)
+         window.stream = remoteVideoStream;
+         const recordButton = document.querySelector('button#record');
+         recordButton.disabled = false;
+      };
+
+      // Setup ice handling 
+      yourConn.onicecandidate = function (event) {
+         if (event.candidate) {
+            send({
+               type: "candidate",
+               candidate: event.candidate
+            });
+         }
+      };
+
+   }
+};
+
+//initiating a call 
+callBtn.addEventListener("click", function () {
+   var callToUsername = callToUsernameInput.value;
+   if (callToUsername.length > 0) {
+      connectedUser = callToUsername;
+      // create an offer 
+      yourConn.createOffer(function (offer) {
+         send({
+            type: "offer",
+            offer: offer
+         });
+         yourConn.setLocalDescription(offer);
+      }, function (error) {
+         alert("Error when creating an offer");
+      });
+
+   }
+});
+
+//when somebody sends us an offer 
+function handleOffer(offer, name) {
+   connectedUser = name;
+   yourConn.setRemoteDescription(new RTCSessionDescription(offer));
+   //create an answer to an offer 
+   yourConn.createAnswer(function (answer) {
+      yourConn.setLocalDescription(answer);
+      send({
+         type: "answer",
+         answer: answer
+      })
+   }, function (error) {
+      alert("Error when creating an answer");
+   });
+};
+//when we got an answer from a remote user
+function handleAnswer(answer) {
+   yourConn.setRemoteDescription(new RTCSessionDescription(answer));
+};
+
+//when we got an ice candidate from a remote user 
+function handleCandidate(candidate) {
+   yourConn.addIceCandidate(new RTCIceCandidate(candidate));
+};
+
+//hang up 
+hangUpBtn.addEventListener("click", function () {
+   send({
+      type: "leave"
+   });
+   handleLeave();
+});
+function handleLeave() {
+   connectedUser = null;
+   remoteVideo.src = null;
+
+   yourConn.close();
+   yourConn.onicecandidate = null;
+   yourConn.onaddstream = null;
 };
--- public/recorder.js
+++ public/recorder.js
@@ -1,45 +1,96 @@
 
-function recorder(){
+/*
+*  Copyright (c) 2015 The WebRTC project authors. All Rights Reserved.
+*
+*  Use of this source code is governed by a BSD-style license
+*  that can be found in the LICENSE file in the root of the source
+*  tree.
+*/
 
-    const videoDOM = $('#localVideoTag')[0]    
-    const stream = videoDOM.captureStream(60);
-var recordedChunks = [];
+// This code is adapted from
+// https://rawgit.com/Miguelao/demos/master/mediarecorder.html
 
-console.log(stream);
-var options = { mimeType: "video/webm; codecs=vp9" };
-mediaRecorder = new MediaRecorder(stream, options);
+'use strict';
 
-mediaRecorder.ondataavailable = handleDataAvailable;
-mediaRecorder.start();
+/* globals MediaRecorder */
+
+let mediaRecorder;
+let recordedBlobs;
+
+
+const recordButton = document.querySelector('button#record');
+recordButton.addEventListener('click', () => {
+  if (recordButton.textContent === 'Start Recording') {
+    startRecording();
+  } else {
+    stopRecording();
+    recordButton.textContent = 'Start Recording';
+    // playButton.disabled = false;
+    downloadButton.disabled = false;
+  }
+});
+
+
+const downloadButton = document.querySelector('button#download');
+downloadButton.addEventListener('click', () => {
+  const blob = new Blob(recordedBlobs, { type: 'video/webm' });
+  const url = window.URL.createObjectURL(blob);
+  const a = document.createElement('a');
+  a.style.display = 'none';
+  a.href = url;
+  a.download = 'test.webm';
+  document.body.appendChild(a);
+  a.click();
+  setTimeout(() => {
+    document.body.removeChild(a);
+    window.URL.revokeObjectURL(url);
+  }, 100);
+});
 
 function handleDataAvailable(event) {
-  console.log("data-available",event.data.size );
-  if (event.data.size > 0) {
-    recordedChunks.push(event.data);
-    console.log(recordedChunks);
-    download();
-  } else {
-      console.log('unable to download')
-    // ...
+  console.log('handleDataAvailable', event);
+  if (event.data && event.data.size > 0) {
+    recordedBlobs.push(event.data);
   }
 }
-function download() {
-  var blob = new Blob(recordedChunks, {
-    type: "video/webm"
-  });
-  var url = URL.createObjectURL(blob);
-  var a = document.createElement("a");
-  document.body.appendChild(a);
-  a.style = "display: none";
-  a.href = url;
-  a.download = "test.webm";
-  a.click();
-  window.URL.revokeObjectURL(url);
+
+function startRecording() {
+  recordedBlobs = [];
+  let options = { mimeType: 'video/webm;codecs=vp9,opus' };
+  if (!MediaRecorder.isTypeSupported(options.mimeType)) {
+    console.error(`${options.mimeType} is not supported`);
+    options = { mimeType: 'video/webm;codecs=vp8,opus' };
+    if (!MediaRecorder.isTypeSupported(options.mimeType)) {
+      console.error(`${options.mimeType} is not supported`);
+      options = { mimeType: 'video/webm' };
+      if (!MediaRecorder.isTypeSupported(options.mimeType)) {
+        console.error(`${options.mimeType} is not supported`);
+        options = { mimeType: '' };
+      }
+    }
+  }
+
+  try {
+    mediaRecorder = new MediaRecorder(window.stream, options);
+  } catch (e) {
+    console.error('Exception while creating MediaRecorder:', e);
+    errorMsgElement.innerHTML = `Exception while creating MediaRecorder: ${JSON.stringify(e)}`;
+    return;
+  }
+
+  console.log('Created MediaRecorder', mediaRecorder, 'with options', options);
+  recordButton.textContent = 'Stop Recording';
+  // playButton.disabled = true;
+  downloadButton.disabled = true;
+  mediaRecorder.onstop = (event) => {
+    console.log('Recorder stopped: ', event);
+    console.log('Recorded Blobs: ', recordedBlobs);
+  };
+  mediaRecorder.ondataavailable = handleDataAvailable;
+  mediaRecorder.start();
+  console.log('MediaRecorder started', mediaRecorder);
 }
 
-// demo: to download after 9sec
-setTimeout(event => {
-  console.log("stopping");
+function stopRecording() {
   mediaRecorder.stop();
-}, 9000);
 }
